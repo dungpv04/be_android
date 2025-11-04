db917d14e3a337d136c9fba8d37f08bd16fa99b6
dungpv04 <vandungpham2004@gmail.com>
2025-11-05 00:01:38 +0700

    allow CORS

--- main.py
+++ main.py
@@ -30,25 +30,8 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",  # React default
-        "http://localhost:5173",  # Vite default
-        "http://localhost:8080",  # Vue default
-        "http://localhost:4200",  # Angular default
-        "http://localhost:5000",  # Flutter web (common)
-        "http://localhost:8000",  # Flutter web (alternative)
-        "http://localhost:9000",  # Flutter web (alternative)
-        "http://localhost:54321", # Flutter web (newer versions)
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:8080",
-        "http://127.0.0.1:4200",
-        "http://127.0.0.1:5000",  # Flutter web
-        "http://127.0.0.1:8000",  # Flutter web
-        "http://127.0.0.1:9000",  # Flutter web
-        "http://127.0.0.1:54321", # Flutter web
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=False,  # Must be False when allow_origins=["*"]
     allow_methods=["*"],  # Allow all methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Allow all headers
 )
